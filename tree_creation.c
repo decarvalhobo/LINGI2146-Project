fd0e1ae100d2fa15822748b6d0fc228eb7ca7e01
alwid <adrien.widart@student.uclouvain.be>
2019-04-16 11:51:02 +0200

    clean code

--- tree_creation.c
+++ tree_creation.c
@@ -1,42 +1,3 @@
-/*
- * Copyright (c) 2007, Swedish Institute of Computer Science.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
-
-/**
- * \file
- *         Testing the broadcast layer in Rime
- * \author
- *         Adam Dunkels <adam@sics.se>
- */
-
 #include "contiki.h"
 #include "net/rime.h"
 #include "random.h"
@@ -54,19 +15,18 @@
 #include <string.h> // for strncpy
 #include <stdbool.h>
 
+#define IS_ROOT                 false
 /* MT == Message Type */
-#define MT_INFORMATION_REQUEST 0
-#define MT_INFORMATION 1
-#define MT_DATA 2
+#define MT_INFORMATION          0
+#define MT_DATA                 1
 
 /* Message format */
 struct Message {
   uint8_t type; 
-  struct Information* info;
 };
 
 /* Global variables required */
-static bool connected_to_root = false;
+static bool connected_to_root = IS_ROOT;
 
 /*---------------------------------------------------------------------------*/
 /* OPTIONAL: Sender history.
@@ -111,9 +71,7 @@ recv_runicast(struct runicast_conn *c, const rimeaddr_t *from, uint8_t seqno)
     e->seq = seqno;
   }
   
-  printf("Unicast received\n");
-  //printf("runicast message received from %d.%d, seqno %d, type %d, root %u:%u\n",
-  //	 from->u8[0], from->u8[1], seqno, message->type, (information->root_address).u8[0], (information->root_address).u8[1]);
+  printf("runicast message received from %d.%d", from->u8[0], from->u8[1]);
   connected_to_root = true;
 }
 static void
@@ -172,7 +130,6 @@ static struct broadcast_conn broadcast;
 PROCESS_THREAD(example_broadcast_process, ev, data)
 {
   static struct etimer et;
-  static struct Message message;
   static bool send_message = false;
 
   PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
@@ -195,13 +152,11 @@ PROCESS_THREAD(example_broadcast_process, ev, data)
     /* If not connected to root, send a broadcast message to get information
        about neighbourhood */
     if (!connected_to_root) {
-      message.type = MT_INFORMATION_REQUEST;
       send_message = true;
     }
   
     /* Send message if asked */
     if (send_message) {
-      packetbuf_copyfrom(&message, sizeof(message));
       broadcast_send(&broadcast);
       printf("broadcast message sent\n");
     }
