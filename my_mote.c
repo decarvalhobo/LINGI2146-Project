cfb25a5cbf9fde08a14aeb48b22af59ee310fc23
alwid <adrien.widart@student.uclouvain.be>
2019-05-02 04:52:38 +0200

    BR discovery

--- my_mote.c
+++ my_mote.c
@@ -17,38 +17,49 @@
 #include <stdint.h>
 
 /* MT == Message Type */
-#define MT_PARENT_INFO          0
-#define MT_STATUS               1
-#define MT_DISCONNECTED         2
-#define MT_DATA                 3
-
-// TODO : quid if node dead ?
-// TODO : comment code
+#define MT_DISCOVERY    0
+#define MT_STATUS       1
 
 typedef struct {
-  rimeaddr_t    addr;
-  int32_t       hops;
-  uint16_t      rssi;
-} Neighbour;
+  rimeaddr_t    parent_addr;
+  uint16_t      parent_rssi;
+  int32_t       hops_to_root;
+} Status;
 
 /* Message formats */
+typedef struct {
+  uint8_t       type;
+} Discovery_Msg;
+
 typedef struct {
   uint8_t       type; 
   uint32_t      hops;
 } Status_Msg;
 
-typedef struct {
-  uint8_t       type;
-  Neighbour     parent;
-} Parent_Info_Msg;
-
 /* Global variables required */
 static bool connected_to_tree = false;
-static Neighbour my_parent;
+static Status my_status;
+
+
+static void send_broadcast(const void* msg, int size);
+static void send_unicast(const void* msg, int size, const rimeaddr_t* to);
+
+static void process_message(const rimeaddr_t *from) {
+  uint8_t* message_type = (uint8_t *) packetbuf_dataptr();
+  switch(*message_type) {
+    case MT_DISCOVERY:
+      printf("Message received from %u.%u : ask for discovery !\n",
+              from->u8[0], from->u8[1]);
+      break;
+    default:
+      printf("Message received");
+  }
+}
 
 /*---------------------------------------------------------------------------*/
 static void recv_uc(struct unicast_conn *c, const rimeaddr_t *from)
 {
+  process_message(from);
 }
 static const struct unicast_callbacks unicast_callbacks = {recv_uc};
 static struct unicast_conn uc;
@@ -61,15 +72,26 @@ AUTOSTART_PROCESSES(&example_broadcast_process);
 static void
 broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from)
 {
+  process_message(from);
 }
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 static struct broadcast_conn broadcast;
 
 /*---------------------------------------------------------------------------*/
+static void send_broadcast(const void* msg, int size){
+  packetbuf_copyfrom(msg, size);
+  broadcast_send(&broadcast);
+}
+static void send_unicast(const void* msg, int size, const rimeaddr_t* to){
+  packetbuf_copyfrom(msg, size);
+  unicast_send(&uc, to);
+}
+
 PROCESS_THREAD(example_broadcast_process, ev, data)
 {
   static struct etimer et;
-  static bool is_root = false;
+  static int    timer = 4;
+  static bool   is_root = false;
 
   PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
   PROCESS_EXITHANDLER(unicast_close(&uc);)
@@ -83,13 +105,24 @@ PROCESS_THREAD(example_broadcast_process, ev, data)
   is_root = rimeaddr_node_addr.u8[0] == 1 && rimeaddr_node_addr.u8[1] == 0;
 
   if (is_root) {
-    my_parent.hops = 0;
+    my_status.hops_to_root = 0;
     connected_to_tree = true;
   } 
 
   while(1) {
-    etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
+    if (!connected_to_tree) timer = 1;
+    else                    timer = 4;
+    
+    etimer_set(&et, CLOCK_SECOND * timer + random_rand() % (CLOCK_SECOND * timer));
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+    if (!connected_to_tree) {
+      Discovery_Msg msg;
+      msg.type = MT_DISCOVERY;
+      send_broadcast((const void*) &msg, sizeof(msg));
+      continue;
+    }
+
   }
 
   PROCESS_END();
