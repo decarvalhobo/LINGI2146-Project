a057fc592ba041b0e3d8d857b6194be373f6d1b0
Instant Contiki <user@instant-contiki.(none)>
2019-05-24 15:10:59 +0200

    Correction client, bug prevent

--- client_pyt.py
+++ client_pyt.py
@@ -8,32 +8,34 @@ client_socket.connect(('localhost', 60001))
 BROKER_ADDR = 'localhost'
 
 def main(client_socket):
-	try:
-		# will hold a single message at a time
-		message=""
-		while 1:			
-		    data = client_socket.recv(1024)
-		    if( data != ""):
-			# data are formatted to end with an EOL
-			if (	data != "\n"):
-				message+=data
-			# when we have everything we process it
-			else :
-				topic_value = message.split(";")
+	# will hold a single message at a time
+	message=""
+	while 1:			
+	    data = client_socket.recv(1024)
+	    if( data != ""):
+		# data are formatted to end with an EOL
+		if (data != "\n"):
+			message+=data
+		# when we have everything we process it
+		else :
+			tmp = message.split("\n") # prevent bug
+			for part in tmp:
+				topic_value = part.split(";")
 				if(len(topic_value) == 4): # ;IP_ADDR;TOPIC;VALUE
 					myCmd = 'mosquitto_pub -h ' + BROKER_ADDR + ' -t ' + topic_value[2] + ' -m ' + topic_value[3]
 					# execute the command that publishes the data received to the specified topic
 					# the mosquitto clients must be installed : apt-get install mosquitto-clients 
 					os.system(myCmd)
-					message=""
 					print "Message sent from "+topic_value[1]+ " for topic : "+ topic_value[2] + ' with value: ' + topic_value[3]
-	except:
-		client_socket.close()
-		print "Stoping..."
+
+				topic_value = []
+			tmp=""
+			message=""
+
 	
 def send_stop(client_socket,topic_name):
 	client_socket.send(u"<w>"+topic_name+"</w>")
 		
 
-send_stop(client_socket,"temperature")
+#send_stop(client_socket,"temperature")
 main(client_socket)	
